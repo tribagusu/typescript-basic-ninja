6cbe70763a4ab5d655ed87f9949ee6f7e2d69937
tribagus <tri.bagus21@gmail.com>
2022-11-10 10:55:15 +0700

    interfaces

--- public/app.js
+++ public/app.js
@@ -1,4 +1,27 @@
 import { Invoice } from "./classes/Invoice.js";
+//# interfaces
+// interface IsPerson {
+//   name: string
+//   age: number
+//   speak(a: string): void
+//   spend(a: number): number
+// }
+// const me: IsPerson = {
+//   name: "tri",
+//   age: 30,
+//   speak(text: string): void {
+//     console.log(text)
+//   },
+//   spend(amount: number): number {
+//     console.log("I spent", amount)
+//     return amount
+//   },
+// }
+// const greetPerson = (person: IsPerson) => {
+//   console.log("hello", person.name)
+// }
+// greetPerson(me)
+// console.log(me)
 const invOne = new Invoice("bagus", "work on the website", 200);
 const invTwo = new Invoice("tri", "work on the website", 190);
 let invoices = [];
